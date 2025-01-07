4cd7834b8be01090714fefb98788e543d0f14b3e
ehsan-mad <saadkhan420000@gmail.com>
2025-01-07 14:18:40 +0600

    put the file in root

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./index.html" , "./src/**/*.{js,jsx,ts,tsx}  "
+
+  ],
   theme: {
     extend: {fontFamily: {
       'ibm-hebrew': ['"IBM Plex Sans Hebrew"', 'sans-serif'],
